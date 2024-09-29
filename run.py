af9c2d206cb545de5966b0dae00c85f261f7fa09
Silvio Martinec <silvio.martinec.ie@gmail.com>
2024-09-29 15:06:13 +0000

    Add few test lines to test Heroku env.

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+import time
+
+# test line for testing Heroku test system to test
+
+print("\nWENDY is not my girlfriend's name, but ...\n")
+
+time.sleep(3)  # Delay for 3 seconds
+
+print("... Welcome to Jamaica and have a nice day\n")
+
+"""
 print("\nMenu:\n")
 print("1. List all records")
 print("2. Create record")
@@ -6,3 +17,4 @@ print("4. Edit record")
 print("5. Show statistics")
 print("_______________________")
 print("[Choose number or Exit]")
+"""

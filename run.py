4e242f6dd08f8b8af8f7f7ad6115d790d1987c5c
Silvio Martinec <silvio.martinec.ie@gmail.com>
2024-09-29 20:12:13 +0000

    Add functional menu loop with options

--- run.py
+++ run.py
@@ -1,20 +1,25 @@
 import time
 
-# test line for testing Heroku test system to test
-
-print("\nWENDY is not my girlfriend's name, but ...\n")
-
-time.sleep(3)  # Delay for 3 seconds
-
-print("... Welcome to Jamaica and have a nice day\n")
-
-"""
-print("\nMenu:\n")
-print("1. List all records")
-print("2. Create record")
-print("3. Delete record")
-print("4. Edit record")
-print("5. Show statistics")
-print("_______________________")
-print("[Choose number or Exit]")
-"""
+menu=True
+while menu:
+    print ("""
+    Menu:
+    1.Add a Record
+    2.Delete a Record
+    3.List the Records
+    4.Exit/Quit
+    """)
+    menu=input("What would you like to do? ") 
+    if menu=="1": 
+      print("\n >>> Record Added!") 
+    elif menu=="2":
+      print("\n >>> Record Deleted!") 
+    elif menu=="3":
+      print("\n >>> Records Listed!") 
+    elif menu=="4":
+      print("\n >>> Closing app!")
+      time.sleep(3)  # Delay for 3 seconds
+      print("\n Goodbye") 
+      break
+    elif menu!="":
+      print("\n >>> Not Valid Choice Try again!") 

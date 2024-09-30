f3dbac4810e4a5cd24c49ffcd9b4a31598486fe6
Silvio Martinec <silvio.martinec.ie@gmail.com>
2024-09-30 12:51:30 +0000

    Add new menu option and change order

--- run.py
+++ run.py
@@ -5,19 +5,22 @@ menu=True
 while menu:
     print ("""
     Menu:
-    1.Add a Record
-    2.Delete a Record
-    3.List the Records
-    4.Exit/Quit
+    1.List the Records
+    2.Add a Record
+    3.Edit a record
+    4.Delete a Record
+    5.Exit/Quit
     """)
     menu=input("What would you like to do? ") 
     if menu=="1": 
-      print("\n >>> Record Added!") 
-    elif menu=="2":
-      print("\n >>> Record Deleted!") 
-    elif menu=="3":
       print("\n >>> Records Listed!") 
+    elif menu=="2":
+      print("\n >>> Record Added!") 
+    elif menu=="3":
+      print("\n >>> Record Edited!") 
     elif menu=="4":
+      print("\n >>> Record Deleted!")
+    elif menu=="5":
       print("\n >>> Closing app!")
       time.sleep(3)  # Delay for 3 seconds
       print("\n Goodbye") 
